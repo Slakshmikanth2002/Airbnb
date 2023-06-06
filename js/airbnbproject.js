ef124914212b5f2922d15ac8d77842bd0f19e0f8
lakshmikanth2002 <129955995+Slakshmikanth2002@users.noreply.github.com>
2023-06-06 19:31:22 +0530

    Add files via upload

---
+++ js/airbnbproject.js
@@ -0,0 +1,74 @@
+// function anywhere(){
+
+//     var ff = document.createElement("div")
+// document.body.append(ff)
+//  ff.style.border="1px solid black"
+//  ff.style.width="500px"
+// ff.textContent="this is my name"
+//     console.log(ff)     
+// }
+
+
+// var para3 = document.getElementById("ww")
+// para3.addEventListener("click",function(){
+// var uu =document.getElementById("div222")
+// console.log("this is content")
+// uu.style.display="block"
+// uu.style.marginTop="80px"
+
+// })
+
+
+
+// var para3 = document.getElementById("ww")
+// para3.addEventListener("click",function(){
+// var zz =document.getElementById("slider")
+// console.log("this is content")
+// zz.style.display="none"
+
+// })
+
+// var para3 = document.getElementById("ww")
+// para3.addEventListener("click",function(){
+// var zz =document.getElementById("slider")
+// console.log("this is content")
+// zz.style.display="none"
+
+// })
+// var para3 = document.getElementById("ww")
+// para3.addEventListener("click",function(){
+// var zz =document.getElementById("slider")
+// console.log("this is content")
+// zz.style.display="none"
+
+// })
+
+
+
+
+
+// // var para2 = document.getElementById("ww")
+// // para2.addEventListener("click",function(){
+// // var mm =document.getElementById("lakshmi")
+// // console.log("this is content")
+// // .style.display="none"
+// // })
+
+
+
+// var bb = document.getElementById("div1")
+
+//  bb.addEventListener("click",function(){
+//     var list =document.getElementById("div222")
+//     console.log("this is content")
+//     list.style.display="none"
+
+//  })
+//  var bb = document.getElementById("div1")
+
+//  bb.addEventListener("click",function(){
+//     var list =document.getElementById("slider")
+//     console.log("this is content")
+//     list.style.display="block"
+
+//  })
---
+++ js/airbnbyourhome.js
@@ -0,0 +1,100 @@
+var para3 = document.getElementById("i111")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline1")
+console.log("this is content")
+uu.style.display="block"
+
+var yy = documnet.getElementById("line")
+
+})
+var para3 = document.getElementById("inlline1")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline1")
+console.log("this is content")
+uu.style.display="none"
+
+
+})
+
+
+var para3 = document.getElementById("i112")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline2")
+console.log("this is content")
+uu.style.display="block"
+
+var yy = documnet.getElementById("line")
+
+})
+var para3 = document.getElementById("inlline2")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline2")
+console.log("this is content")
+uu.style.display="none"
+
+
+})
+
+
+var para3 = document.getElementById("i113")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline3")
+console.log("this is content")
+uu.style.display="block"
+
+var yy = documnet.getElementById("line")
+
+
+})
+var para3 = document.getElementById("inlline3")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline3")
+console.log("this is content")
+uu.style.display="none"
+
+
+})
+
+
+var para3 = document.getElementById("i114")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline4")
+console.log("this is content")
+uu.style.display="block"
+
+var yy = documnet.getElementById("line")
+
+
+})
+var para3 = document.getElementById("inlline4")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline4")
+console.log("this is content")
+uu.style.display="none"
+
+
+})
+
+
+var para3 = document.getElementById("i115")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline5")
+console.log("this is content")
+uu.style.display="block"
+
+var yy = documnet.getElementById("line")
+yy.style.height="117px"
+
+})
+var para3 = document.getElementById("inlline5")
+para3.addEventListener("click",function(){
+var uu =document.getElementById("inlline5")
+console.log("this is content")
+uu.style.display="none"
+
+
+})
+
+
+
+
